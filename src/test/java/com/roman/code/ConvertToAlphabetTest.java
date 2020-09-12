d1344492d6e6d24e4bd5c5785742311c2f878280
Gonzalo Mendoza <gonzalo.mendoza@globant.com>
2020-09-12 17:13:32 -0300

    Testing all cases

--- src/test/java/com/roman/code/ConvertToAlphabetTest.java
+++ src/test/java/com/roman/code/ConvertToAlphabetTest.java
@@ -8,20 +8,18 @@ import com.roman.code.domain.Alphabet;
 import javax.naming.OperationNotSupportedException;
 import org.junit.jupiter.api.Test;
 
-import java.util.stream.IntStream;
-
 public class ConvertToAlphabetTest {
   private Alphabet roman = Alphabet.roman();
   private Alphabet facundo =
-            Alphabet.builder()
-            .One("F")
-            .Five("A")
-            .Ten("C")
-            .Fifty("U")
-            .OneHundred("N")
-            .FiveHundred("D")
-            .Thousand("O")
-            .build();
+      Alphabet.builder()
+          .One("F")
+          .Five("A")
+          .Ten("C")
+          .Fifty("U")
+          .OneHundred("N")
+          .FiveHundred("D")
+          .Thousand("O")
+          .build();
 
   @Test
   public void shouldFailIfNumberIsNull() {
@@ -69,26 +67,32 @@ public class ConvertToAlphabetTest {
     assertEquals("MMMCMXCIX", ConvertToAlphabet.fromArabic(3999, roman));
   }
 
-    @Test
-    public void shouldPassWhenAllNumbersAreCorrect() throws OperationNotSupportedException {
-        for(int i=1; i < 4000; i++) {
-            String rNumber = ConvertToAlphabet.fromArabic(i, roman);
-            Integer aNumber = ConvertToArabic.fromRoman(rNumber);
-            assertEquals(i, aNumber);
-        }
-    }
+  @Test
+  public void shouldPassWhenFacundoConversionIsOK() throws OperationNotSupportedException {
+    assertEquals("ONN", ConvertToAlphabet.fromArabic(1200, facundo));
+    assertEquals("ONOCFA", ConvertToAlphabet.fromArabic(1914, facundo));
+    assertEquals("CFA", ConvertToAlphabet.fromArabic(14, facundo));
+    assertEquals("C", ConvertToAlphabet.fromArabic(10, facundo));
+    assertEquals("FA", ConvertToAlphabet.fromArabic(4, facundo));
+    assertEquals("DUFA", ConvertToAlphabet.fromArabic(554, facundo));
+    assertEquals("NNNFFF", ConvertToAlphabet.fromArabic(303, facundo));
+    assertEquals("NOFC", ConvertToAlphabet.fromArabic(909, facundo));
+    assertEquals("OOO", ConvertToAlphabet.fromArabic(3000, facundo));
+    assertEquals("OOONOCNFC", ConvertToAlphabet.fromArabic(3999, facundo));
+  }
 
-    @Test
-    public void shouldPassWhenFacundoConversionIsOK() throws OperationNotSupportedException {
-        assertEquals("ONN", ConvertToAlphabet.fromArabic(1200, facundo));
-        assertEquals("ONOCFA", ConvertToAlphabet.fromArabic(1914, facundo));
-        assertEquals("CFA", ConvertToAlphabet.fromArabic(14, facundo));
-        assertEquals("C", ConvertToAlphabet.fromArabic(10, facundo));
-        assertEquals("FA", ConvertToAlphabet.fromArabic(4, facundo));
-        assertEquals("DUFA", ConvertToAlphabet.fromArabic(554, facundo));
-        assertEquals("NNNFFF", ConvertToAlphabet.fromArabic(303, facundo));
-        assertEquals("NOFC", ConvertToAlphabet.fromArabic(909, facundo));
-        assertEquals("OOO", ConvertToAlphabet.fromArabic(3000, facundo));
-        assertEquals("OOONOCNFC", ConvertToAlphabet.fromArabic(3999, facundo));
+  @Test
+  public void shouldPassWhenAllNumbersAreCorrect() throws OperationNotSupportedException {
+    for (int i = 1; i < 4000; i++) {
+        // Check all roman numbers
+      String rNumber = ConvertToAlphabet.fromArabic(i, roman);
+      Integer aNumber = ConvertToArabic.fromRoman(rNumber);
+      assertEquals(i, aNumber);
+
+      // Check all custom alphabet numbers
+      rNumber = ConvertToAlphabet.fromArabic(i, facundo);
+      aNumber = ConvertToArabic.fromAlphabet(rNumber, facundo);
+      assertEquals(i, aNumber);
     }
+  }
 }
